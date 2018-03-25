38a35584ea0c3df651107b40586cbe46e109e126
luoming <1661391985@qq.com>
2018-03-25 23:19:24 +0800

    内部改变数组或对象 PureComponent 将不起作用

--- src/components/PureComponent/index.js
+++ src/components/PureComponent/index.js
@@ -1,26 +1,47 @@
 import React from 'react';
+import {Button} from 'antd';
 //易变数据不能使用一个引用
 //不变数据使用一个引用
-export default class Pure extends React.PureComponent{
-  constructor(props){
+export default class Pure extends React.PureComponent {
+  constructor(props) {
     super(props);
+    this.state = {
+      color: 'red',
+      array:[8,9]
+    }
   }
-  render(){
+
+  click = () => {
+    let {array}=this.state;
+    //内部改变数组 PureComponent不起作用
+    array.push(10);
+    this.setState({color:'blue',array});
+  }
+
+  render() {
     console.log('pureComponent');
-    let {list,data,obj}=this.props;
+    let {list, data, obj} = this.props;
+    let {color,array} = this.state;
     return (
       <div>
-       <div>
-         'pureComponent'
-       </div>
+        <div>
+          'pureComponent'
+        </div>
         <div>{data + 1}</div>
         {
-          list.map((v,k)=>{
+          list.map((v, k) => {
             return <div key={k}>{v}</div>
           })
         }
         <div>
           {obj.name} ---{obj.age}
+        </div>
+        <Button onClick={this.click}>Pure内部点击</Button>
+        <div>{color}</div>
+        <div>
+          {array.map((v,k)=>{
+            return <div key={k}>{v}</div>
+          })}
         </div>
       </div>
     )
