35397a49118c440b830a75669633281457dddcad
luoming <1661391985@qq.com>
2018-04-10 00:00:18 +0800

    rebase

--- src/view/Home/index.js
+++ src/view/Home/index.js
@@ -8,7 +8,7 @@ export default class Home extends React.PureComponent{
       <div>
         <img src="" alt=""/>
         <h1>这是一个本人练手的项目，用来熟悉react及其他的知识点，其中包括但并不限于defaultProps，context，mobx，mobx-react，
-          mirror，PureComponent,webworker，装饰器函数==后续补充
+          mirror，PureComponent,webworker，装饰器函数==后续补充()
         </h1>
       </div>
     )
